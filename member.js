d3fce8a64388892b861223b98aecf2694e493953
ReneOlesen <162693356+ReneOlesen@users.noreply.github.com>
2025-03-04 07:02:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 20,
+        skills: ['JavaScript', 'React', 'Node'],
+        details: {
+            address: '123 Street Name',
+            city: 'City Name'
+        }
+    }
+}
